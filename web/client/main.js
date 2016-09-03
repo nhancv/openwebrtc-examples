6a68d14dccc90cc5d9ba80136c4743a2379abf7e
Nhan Cao <nhancv1992@gmail.com>
2016-09-03 12:49:39 +0700

    broadcast call

--- web/client/main.js
+++ web/client/main.js
@@ -6,7 +6,7 @@ if (isMozilla) {
     window.RTCSessionDescription = window.mozRTCSessionDescription;
     window.RTCIceCandidate = window.mozRTCIceCandidate;
 }
-
+var broadCastId = "000000000000";
 var selfView;
 var remoteView;
 var callButton;
@@ -19,17 +19,21 @@ var chatDiv;
 var chatText;
 var chatButton;
 var chatCheckBox;
-var brCheckBox;
+var broadcastCheckBox;
+var conferenceCheckBox;
+var callMode; //0: p2p, 1: broadcast, 2: conference
 var peerIds = [];
 var peerChannels = {};
-var peer; //temporary
-var pc; //temporary
-var channel; //temporary
-var lastPeerId;
+var activePeerId;
 
-if (!window.hasOwnProperty("orientation"))
-    window.orientation = -90;
+if (!window.hasOwnProperty("orientation")) {
+    // window.orientation = -90;
+}
 
+var offerOptions = {
+    offerToReceiveAudio: true,
+    offerToReceiveVideo: true
+};
 // must use 'url' here since Firefox doesn't understand 'urls'
 var configuration = {
     "iceServers": [
@@ -56,7 +60,8 @@ window.onload = function () {
     chatButton = document.getElementById("chat_but");
     chatDiv = document.getElementById("chat_div");
     chatCheckBox = document.getElementById("chat_cb");
-    brCheckBox = document.getElementById("broadcast_cb");
+    broadcastCheckBox = document.getElementById("broadcast_cb");
+    conferenceCheckBox = document.getElementById("conference_cb");
 
     // if browser doesn't support DataChannels the chat will be disabled.
     if (webkitRTCPeerConnection.prototype.createDataChannel === undefined) {
@@ -86,7 +91,18 @@ window.onload = function () {
             return;
         }
 
-        audioCheckBox.disabled = videoCheckBox.disabled = chatCheckBox.disabled = joinButton.disabled = true;
+        callMode = 0;
+        if (broadcastCheckBox.checked) {
+            callMode = 1;
+            offerOptions = {
+                offerToReceiveAudio: false,
+                offerToReceiveVideo: false
+            };
+        } else if (conferenceCheckBox.checked) {
+            callMode = 2;
+        }
+
+        broadcastCheckBox.disable = conferenceCheckBox.disable = audioCheckBox.disabled = videoCheckBox.disabled = chatCheckBox.disabled = joinButton.disabled = true;
 
         // only chat
         if (!(videoCheckBox.checked || audioCheckBox.checked)) peerJoin();
@@ -107,22 +123,10 @@ window.onload = function () {
 
             // another peer has joined our session
             signalingChannel.onpeer = function (evt) {
-                callButton.disabled = false;
+                if(peerIds[broadCastId]!=-1 && callMode==1){
+                    callButton.disabled = false;
+                }
                 shareView.style.visibility = "hidden";
-                var peerChannel = peerChannels[peerIds[peerIds.length - 1]];
-                if (peerChannel == undefined || !peerChannel.hasOwnProperty("peer")) peerChannels[peerIds[peerIds.length - 1]] = {
-                    peer: evt.peer,
-                    pc: null
-                };
-                peerChannels[peerIds[peerIds.length - 1]]["peer"].onmessage = handleMessage;
-                peerChannels[peerIds[peerIds.length - 1]]["peer"].ondisconnect = function () {
-                    callButton.disabled = true;
-                    remoteView.style.visibility = "hidden";
-                    var pc = peerChannels[lastPeerId]["pc"];
-                    if (pc)
-                        pc.close();
-                    pc = null;
-                };
             };
         }
 
@@ -136,15 +140,14 @@ window.onload = function () {
                 // .. show it in a self-view
                 selfView.src = URL.createObjectURL(stream);
                 // .. and keep it to be sent later
-                localStream = stream;
-
-                joinButton.disabled = true;
-                chatButton.disabled = true;
 
                 if (videoCheckBox.checked)
                     selfView.style.visibility = "visible";
                 else if (audioCheckBox.checked && !(chatCheckBox.checked))
                     audioOnlyView.style.visibility = "visible";
+                localStream = stream;
+                joinButton.disabled = true;
+                chatButton.disabled = true;
 
                 peerJoin();
             }, logError);
@@ -166,135 +169,96 @@ window.onload = function () {
     }
 };
 
-// handle signaling messages received from the other peer
-function handleMessage(evt) {
-    var message = JSON.parse(evt.data);
-    if (!peerChannels[lastPeerId]["pc"] && (message.sessionDescription || message.candidate))
-        start(false);
-    if (message.sessionDescription) {
-        peerChannels[lastPeerId]["pc"].setRemoteDescription(new RTCSessionDescription({
-            "sdp": SDP.generate(message.sessionDescription),
-            "type": message.type
-        }), function () {
-            // if we received an offer, we need to create an answer
-            if (peerChannels[lastPeerId]["pc"].remoteDescription.type == "offer")
-                peerChannels[lastPeerId]["pc"].createAnswer(function (desc) {
-                    peerChannels[lastPeerId]["pc"].setLocalDescription(desc, function () {
-                        var sessionDescription = SDP.parse(peerChannels[lastPeerId]["pc"].localDescription.sdp);
-                        peerChannels[lastPeerId]["peer"].send(JSON.stringify({
-                            "sessionDescription": sessionDescription,
-                            "type": peerChannels[lastPeerId]["pc"].localDescription.type
-                        }, null, 2));
-                        var logMessage = "localDescription set and sent to peer, type: " + peerChannels[lastPeerId]["pc"].localDescription.type
-                            + ", sessionDescription:\n" + JSON.stringify(sessionDescription, null, 2);
-                        console.log(logMessage);
-                    }, logError);
-                }, logError);
-        }, logError);
-    } else if (!isNaN(message.orientation) && remoteView) {
-        var transform = "rotate(" + message.orientation + "deg)";
-        remoteView.style.transform = remoteView.style.webkitTransform = transform;
-    } else {
-        var d = message.candidate.candidateDescription;
-        message.candidate.candidate = "candidate:" + [
-                d.foundation,
-                d.componentId,
-                d.transport,
-                d.priority,
-                d.address,
-                d.port,
-                "typ",
-                d.type,
-                d.relatedAddress && ("raddr " + d.relatedAddress),
-                d.relatedPort && ("rport " + d.relatedPort),
-                d.tcpType && ("tcptype " + d.tcpType)
-            ].filter(function (x) {
-                return x;
-            }).join(" ");
-        peerChannels[lastPeerId]["pc"].addIceCandidate(new RTCIceCandidate(message.candidate), function () {
-        }, logError);
-    }
-}
 // call start() to initiate
 function start(isInitiator) {
     callButton.disabled = true;
-    for (var i = 0; i < peerIds.length; i++) {
-        peerChannels[peerIds[i]]["pc"] = new webkitRTCPeerConnection(configuration);
-        // send any ice candidates to the other peer
-        peer = peerChannels[peerIds[i]]["peer"];
-        peerChannels[peerIds[i]]["pc"].onicecandidate = function (evt) {
-            if (evt.candidate) {
-                var s = SDP.parse("m=application 0 NONE\r\na=" + evt.candidate.candidate + "\r\n");
-                var candidateDescription = s.mediaDescriptions[0].ice.candidates[0];
-                peer.send(JSON.stringify({
-                    "candidate": {
-                        "candidateDescription": candidateDescription,
-                        "sdpMLineIndex": evt.candidate.sdpMLineIndex
-                    }
-                }, null, 2));
-                console.log("candidate emitted: " + JSON.stringify(candidateDescription, null, 2));
-            }
-        };
-        // start the chat
-        if (chatCheckBox.checked) {
-            if (isInitiator) {
-                peerChannels[peerIds[i]]["channel"] = peerChannels[peerIds[i]]["pc"].createDataChannel("chat");
-                setupChat();
-            } else {
-                channel = peerChannels[peerIds[i]]["channel"];
-                peerChannels[peerIds[i]]["pc"].ondatachannel = function (evt) {
-                    channel = evt.channel;
-                    setupChat();
-                };
-                peerChannels[peerIds[i]]["channel"] = channel;
-            }
+    if (callMode == 1) {
+        for (var i = 0; i < peerIds.length; i++) {
+            createPeerConnection(isInitiator, peerChannels[peerIds[i]]);
         }
-
-        // once the remote stream arrives, show it in the remote video element
-        peerChannels[peerIds[i]]["pc"].onaddstream = function (evt) {
-            remoteView.src = URL.createObjectURL(evt.stream);
-            if (videoCheckBox.checked)
-                remoteView.style.visibility = "visible";
-            else if (audioCheckBox.checked && !(chatCheckBox.checked))
-                audioOnlyView.style.visibility = "visible";
-            sendOrientationUpdate();
-        };
-
-        if (audioCheckBox.checked || videoCheckBox.checked) {
-            peerChannels[peerIds[i]]["pc"].addStream(localStream);
+    } else if (callMode == 0) {
+        if (peerIds.indexOf(broadCastId) == -1) {
+            createPeerConnection(isInitiator, peerChannels[activePeerId]);
+        } else {
+            createPeerConnection(isInitiator, peerChannels[broadCastId]);
         }
-
-        if (isInitiator){
-            pc = peerChannels[peerIds[i]]["pc"];
-            peer = peerChannels[peerIds[i]]["peer"];
-            console.log(peer);
-            peerChannels[peerIds[i]]["pc"].createOffer(function (desc) {
-                pc.setLocalDescription(desc, function () {
-                    var sessionDescription = SDP.parse(pc.localDescription.sdp);
-                    peer.send(JSON.stringify({
-                        "sessionDescription": sessionDescription,
-                        "type": pc.localDescription.type
-                    }, null, 2));
-                    var logMessage = "localDescription set and sent to peer, type: " + pc.localDescription.type
-                        + ", sessionDescription:\n" + JSON.stringify(sessionDescription, null, 2);
-                    console.log(logMessage);
-                }, logError);
-            }, logError);
-            peerChannels[peerIds[i]]["pc"] = pc;
-        }
-
     }
-
+    // console.log(peerChannels);
 }
 
-function sendOrientationUpdate() {
+function createPeerConnection(isInitiator, peerChannel) {
+    if (peerChannel["pc"] != null) return;
+    peerChannel["pc"] = new webkitRTCPeerConnection(configuration);
+    // send any ice candidates to the other peer
+    peerChannel["pc"].onicecandidate = function (evt) {
+        if (evt.candidate) {
+            var s = SDP.parse("m=application 0 NONE\r\na=" + evt.candidate.candidate + "\r\n");
+            var candidateDescription = s.mediaDescriptions[0].ice.candidates[0];
+            peerChannel["peer"].send(JSON.stringify({
+                "candidate": {
+                    "candidateDescription": candidateDescription,
+                    "sdpMLineIndex": evt.candidate.sdpMLineIndex
+                }
+            }, null, 2));
+            // console.log("candidate emitted: " + JSON.stringify(candidateDescription, null, 2));
+        }
+    };
+    // start the chat
+    if (chatCheckBox.checked) {
+        if (isInitiator) {
+            peerChannel["channel"] = peerChannel["pc"].createDataChannel("chat");
+            setupChat();
+        } else {
+            peerChannel["pc"].ondatachannel = function (evt) {
+                peerChannel["channel"] = evt.channel;
+                setupChat();
+            };
+        }
+    }
+
+    // once the remote stream arrives, show it in the remote video element
+    peerChannel["pc"].onaddstream = function (evt) {
+        remoteView.src = URL.createObjectURL(evt.stream);
+        if (videoCheckBox.checked)
+            remoteView.style.visibility = "visible";
+        else if (audioCheckBox.checked && !(chatCheckBox.checked))
+            audioOnlyView.style.visibility = "visible";
+        sendOrientationUpdate(peerChannel["peer"]);
+    };
+
+    if (audioCheckBox.checked || videoCheckBox.checked) {
+        peerChannel["pc"].addStream(localStream);
+    }
+
+    if (isInitiator) {
+        peerChannel["pc"].createOffer(function (offer) {
+            peerChannel["pc"].setLocalDescription(offer, function () {
+                var sessionDescription = SDP.parse(peerChannel["pc"].localDescription.sdp);
+                peerChannel["peer"].send(JSON.stringify({
+                    "sessionDescription": sessionDescription,
+                    "type": peerChannel["pc"].localDescription.type
+                }, null, 2));
+                // var logMessage = "localDescription set and sent to peer, type: " + peerChannel["pc"].localDescription.type
+                //     + ", sessionDescription:\n" + JSON.stringify(sessionDescription, null, 2);
+                // console.log(logMessage);
+            }, logError);
+        }, logError, offerOptions);
+    }
+}
+
+function sendOrientationUpdate(peer) {
     peer.send(JSON.stringify({"orientation": window.orientation + 90}));
 }
 
 window.onorientationchange = function () {
-    var peer = peerChannels[lastPeerId]["peer"];
-    if (peer)
-        sendOrientationUpdate();
+    for (var i = 0; i < peerIds.length; i++) {
+        if (peerIds[i] != broadCastId) {
+            var peer = peerChannels[peerIds[i]]["peer"];
+            if (peer) {
+                sendOrientationUpdate(peer);
+            }
+        }
+    }
 
     if (selfView) {
         var transform = "rotate(" + (window.orientation + 90) + "deg)";
@@ -320,7 +284,7 @@ function log(msg) {
 
 // setup chat
 function setupChat() {
-    var channel = peerChannels[lastPeerId]["channel"];
+    var channel = peerChannels[activePeerId]["channel"];
     channel.onopen = function () {
         chatDiv.style.visibility = "visible";
         chatText.style.visibility = "visible";
